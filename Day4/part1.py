aaa6c77f5c1dc7f62569766aa258797280e4b757
Tortuga <arthur.ergin.16@gmail.com>
2025-12-04 13:45:47 +0100

    day 4 part 1and 2 working

---
+++ Day4/part1.py
@@ -0,0 +1,37 @@
+def main():
+    problemInput = open("input.txt", "r")
+    diagram = [line.strip() for line in problemInput.readlines()]
+    height = len(diagram)
+    width = len(diagram[0])
+
+    numValidRolls = 0
+    for y in range(height):
+        for x in range(width):
+            cell = diagram[y][x]
+
+            if cell == "@":
+                numRolls = 0
+
+                for dx, dy in (
+                    (0, 1),
+                    (1, 1),
+                    (1, 0),
+                    (-1, 1),
+                    (-1, 0),
+                    (-1, -1),
+                    (0, -1),
+                    (1, -1),
+                ):
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < width and 0 <= ny < height:
+                        if diagram[ny][nx] == "@":
+                            numRolls += 1
+
+                if numRolls < 4:
+                    numValidRolls += 1
+
+    print(numValidRolls)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Day4/part2.py
@@ -0,0 +1,46 @@
+def main():
+    problemInput = open("input.txt", "r")
+    diagram = [list(line.strip()) for line in problemInput.readlines()]
+    nextDiagram = [list(line) for line in diagram]
+    height = len(diagram)
+    width = len(diagram[0])
+
+    numValidRolls = 0
+
+    while True:
+        for y in range(height):
+            for x in range(width):
+                cell = diagram[y][x]
+
+                if cell == "@":
+                    numAdjRolls = 0
+
+                    for dx, dy in (
+                        (0, 1),
+                        (1, 1),
+                        (1, 0),
+                        (-1, 1),
+                        (-1, 0),
+                        (-1, -1),
+                        (0, -1),
+                        (1, -1),
+                    ):
+                        nx, ny = x + dx, y + dy
+                        if 0 <= nx < width and 0 <= ny < height:
+                            if diagram[ny][nx] == "@":
+                                numAdjRolls += 1
+
+                    if numAdjRolls < 4:
+                        numValidRolls += 1
+                        nextDiagram[y][x] = "."
+
+        if diagram == nextDiagram:
+            break
+
+        diagram = [list(line) for line in nextDiagram]
+
+    print(numValidRolls)
+
+
+if __name__ == "__main__":
+    main()
